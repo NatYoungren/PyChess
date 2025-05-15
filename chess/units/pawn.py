e4e61dd9bb0f79e3c0d79f790054c4edb4036b4e
NatYoungren <natahyoungren@gmail.com>
2025-05-15 14:51:48 -0400

    Cleaned up pawn actions

--- chess/units/pawn.py
+++ chess/units/pawn.py
@@ -13,33 +13,24 @@ class PawnMoveOnly(Action):
     """
     Represents a move action for a pawn.
     """
-    # TODO: Implement this!
-    #       Have a helper for 'moves till blocked' with flag for capture or not.
-    # OPTIONS_FIRST_TURN: Tuple[Vector] = (D.f, D.f*2)
-    # OPTIONS: Tuple[Vector] = (D.f)
     def update(self):
         super().update()
-        
-        tdata = self.get_line(D.f,
+        for pos, t, p in self.get_line(D.f,
                               length=2 if self.move_count == 0 else 1,
-                              enemy_ok=False)
-        for pos, t, p in tdata:
+                              enemy_ok=False):
             self.outcomes[t] = Move(self.piece, pos)
         
 class PawnCaptureOnly(Action):
     """
     Represents a capture action for a pawn.
     """
-    CAPTURE_VECTORS: Tuple[Vector] = (D.f_l, D.f_r) # Left and right diagonal
     def update(self):
         super().update()
-        
-        for v in self.CAPTURE_VECTORS:
-            pos, t, p = self.at_vec(v)
-            if t is None: continue # OOB tile
-            if p is None: continue # No piece to capture
-            if p.loyalty == self.loyalty: continue # No friendly fire
-            self.outcomes[t] = Capture(self.piece, pos, p)
+        for v in (D.f_l, D.f_r):
+            for pos, t, p in self.get_line(v,
+                                          length=1,
+                                          can_move=False):
+                self.outcomes[t] = Capture(self.piece, pos, p)
 
 
 class Pawn(ChessPiece):
