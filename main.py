615b5175ed55cfd1d0b208d9a1966286e27c507d
NatYoungren <natahyoungren@gmail.com>
2025-05-15 14:52:40 -0400

    Automated enemy turn

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import pygame as pg
 from globalref import OBJREF
 
 
-# board_csv = 'default_board.csv'
+board_csv = 'default_board.csv'
 # piece_csv = 'default_pieces.csv'
 
 # board_csv = 'void_board.csv'
 # piece_csv = 'void_pieces.csv'
 
-board_csv = 'obstacle_board.csv'
-piece_csv = 'obstacle_pieces.csv'
+# board_csv = 'obstacle_board.csv'
+# piece_csv = 'obstacle_pieces.csv'
+
+piece_csv = 'summoner_pieces.csv'
 
 board_dir = 'chess/board_csvs'
 piece_dir = 'chess/piece_csvs'
@@ -38,10 +40,33 @@ board.update()
 # game loop
 target_fps = 60
 game_clock = pg.time.Clock()
+
+# Auto turn variables
+auto_turn_timer = -1
+auto_tile = None
+auto_oc = None
+
 while ih.running:
+
+    # TODO: Need a game manager and bot classes.
+    if not board.controlled_turn:
+        if auto_turn_timer <= 0:
+            auto_tile, auto_oc = board.random_outcome()
+            if auto_oc is not None:
+                ui.h_tile = auto_tile
+                ui.s_piece = auto_oc.piece
+                auto_turn_timer = 60
+            else:
+                auto_turn_timer = 0
+        auto_turn_timer -= 1
+        if auto_turn_timer <= 0:
+            board.realize(auto_oc) if auto_oc is not None else board.next_turn()
+            ui.s_tile = None
+        
+        
     ih.update_mousepos()
     for event in pg.event.get(): 
-        ih.handle_event(event)
+        ih.handle_event(event, locked_board=auto_turn_timer > 0)
     ui.draw()
     game_clock.tick(target_fps)
 
