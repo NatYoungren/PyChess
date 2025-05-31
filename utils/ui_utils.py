32d5a0e5632ab0eec0b91bd645e608e55e255d13
NatYoungren <natahyoungren@gmail.com>
2025-05-31 14:09:35 -0400

    Added numpy types to transform typecheck

--- utils/ui_utils.py
+++ utils/ui_utils.py
@@ -6,20 +6,21 @@ from typing import Optional, Union, Tuple
 def sprite_transform(img: pg.Surface,
                      randomflip: bool = False,
                      randomrotate: bool = False,
-                     rotate_by: Optional[int]=None,
+                     rotate_by: Optional[int] = None,
                      size: Union[None, int, Tuple[int, int]] = None) -> pg.Surface:
     """
-    Transform a sprite to the given size.
+    Transform a sprite to the given size (optionally flipped or rotated).
     """
     if randomflip: img = pg.transform.flip(img, *np.random.randint(0, 2, 2))
     if randomrotate:
         img = pg.transform.rotate(img, np.random.randint(0, 4)*90)
     
-    elif isinstance(rotate_by, int):
+    elif isinstance(rotate_by, (int, np.int64)):
         img = pg.transform.rotate(img, rotate_by*90)
         
     if size is not None:
-        if isinstance(size, int): size = (size, size)
+        if isinstance(size, (int, np.int64)):
+            size = (size, size)
         img = pg.transform.scale(img, size)
     
     return img
